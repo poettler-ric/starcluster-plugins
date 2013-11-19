cb197a7a1b8df28b16172c60ae5c047b992c8358
Richard Poettler <richard.poettler@gmail.com>
2013-11-19 16:12:32 +0000

    switched from a pure msmtp approach to a more general (mailx) solution

--- mailsetup.py
+++ mailsetup.py
@@ -2,16 +2,18 @@ from starcluster.clustersetup import ClusterSetup
 from starcluster.logger import log
 from os import path
 
-class MsmtpInstaller(ClusterSetup):
+class MailSetup(ClusterSetup):
 	def __init__(self):
 		None
 
 	def run(self, nodes, master, user, user_shell, volumes):
-		log.info("installing msmtp on master and copying ~/.msmtprc")
+		log.info("installing msmtp and mailx")
 		# TODO: switch to package_install - in the current release
 		# package_provider is broken, so we have to stick with
 		# apt_install
-		#master.package_install("msmtp")
-		master.apt_install("msmtp")
+		#master.package_install("msmtp bsd-mailx")
+		master.apt_install("msmtp bsd-mailx")
+		log.info("setting up rc files")
 		master.ssh.switch_user(user)
 		master.ssh.put(path.expanduser("~/.msmtprc"))
+		master.ssh.execute("echo set sendmail=`which msmtp` >>~/.mailrc")
