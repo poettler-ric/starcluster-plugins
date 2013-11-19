9790a57a20ee8b364daa4d2b7fdf75297e9f182b
Richard Poettler <richard.poettler@gmail.com>
2013-11-19 13:42:01 +0000

    switched to starcluster apt_* commands

--- openfoam.py
+++ openfoam.py
@@ -12,15 +12,14 @@ class OpenFoamInstaller(ClusterSetup):
 	def aptInstallOpenFoam(self, instance):
 		repository = "deb http://www.openfoam.org/download/ubuntu `lsb_release -cs` main"
 		listFile = "/etc/apt/sources.list.d/openfoam.list"
-		installOptions = "-y --force-yes"
 		package = "openfoam222"
 
 		log.info("installing %s on %s" % (package, instance.alias))
 		
 		# install the package
 		instance.ssh.execute("echo %s > %s" % (repository, listFile))
-		instance.ssh.execute("apt-get update")
-		instance.ssh.execute("apt-get %s install %s" % (installOptions, package))
+		instance.apt_command("update")
+		instance.apt_install(package)
 
 		# set the environment
 		instance.ssh.execute("echo . /opt/%s/etc/bashrc >> ~/.bashrc" % package)
